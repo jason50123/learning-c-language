da9109364a748d77aadde8f27fce922ebe4ae9d7
jason50123 <chen.jason50123@gmail.com>
2023-04-16 14:12:40 +0800

    study

---
+++ practice/ch09/recur.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int up_and_down(int);
+
+int main(void){
+    up_and_down(1);
+    return 0;
+}
+
+int up_and_down(int n){
+    printf("Level %d: n location %p\n",n, &n);
+    if(n < 4)
+        up_and_down(n+1);
+    printf("Level %d: n location %p\n", n, &n);
+
+}
