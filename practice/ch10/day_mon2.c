a1447e79aa8d1b38d4b3979ed2734da9f626be4b
jason50123 <chen.jason50123@gmail.com>
2023-04-16 14:12:28 +0800

    study

--- practice/ch10/day_mon2.c
+++ practice/ch10/day_mon2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
-    const int days[] ={31, 28, 31, 30, 31, 30, 31, 31, 30, 31};
+    const int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31};
     int index;
     for(index = 0; index < sizeof days/sizeof days[0]; index++){
         printf("Month %2d has %d days.\n", index+1, days[index]);
